4ca31d82448cbda75f3cf5ea3737059f8194c7da
imsami-3 <57286288+imsami-3@users.noreply.github.com>
2019-11-05 14:34:20 +0530

    Update and rename src/main/java/com/viva/Calculator/App.java to src/main/java/com/viva/electricity/ElectricityBillingApp.java

---
+++ src/main/java/com/viva/electricity/ElectricityBillingApp.java
@@ -0,0 +1,64 @@
+package com.viva.Calculator;
+
+
+public class ElectrictyBillingApp {
+
+	public static double getBillAmount(int previousMeterReading, int presentMeterReading) throws IllegalArgumentException
+	{
+
+		Double totalAmount = 0.00;
+		int netMeterReading = 0;
+
+		if(previousMeterReading<presentMeterReading)
+		{
+			netMeterReading  = presentMeterReading - previousMeterReading;
+			Double currentBillAmount = 0.00;
+			Double tax = 0.00;
+			int remainingUnits = 0;
+			Double first20 = 0.00;
+			Double next30 = 0.00;
+			Double next50 = 0.00;
+			Double above100 = 0.00;
+
+			if(netMeterReading>0 && netMeterReading<=20)
+			{
+				first20 = presentMeterReading * 2.00;
+			}
+			else
+			{
+				first20 = 20 * 2.00;
+				remainingUnits = netMeterReading - 20;
+				if(netMeterReading>=21 && netMeterReading<=50)
+				{
+					next30 = remainingUnits * 3.00;
+				}
+				else 
+				{
+					next30 = 30 * 3.00;
+					remainingUnits = remainingUnits - 30;
+					if(netMeterReading>=51 && netMeterReading<=100)
+					{
+						next50 = remainingUnits * 3.50;
+					}
+					else
+					{
+						next50 = 50 * 3.50;
+						remainingUnits = remainingUnits-50;
+						if(netMeterReading>100)
+						{
+							above100 = remainingUnits * 4.00;
+						}
+					}
+				}
+			}
+			currentBillAmount = first20 + next30 + next50 + above100;
+			tax = ((currentBillAmount)*12.50) / 100;
+			totalAmount = currentBillAmount + tax;
+		}
+		else
+		{
+			throw new IllegalArgumentException();
+		}
+		return totalAmount;
+	}
+}
